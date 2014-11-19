aea0534af04200b5dcafb526953d9354e11ecbbe
Tomas Prokes <tomas.prokes@centrum.cz>
2014-11-19 16:30:41 +0100

    DBHandlerTest.java - modified

--- src/backend/DBHandlerTest.java
+++ src/backend/DBHandlerTest.java
@@ -6,10 +6,16 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.mongodb.util.JSON;
+
 public class DBHandlerTest {
 
 	@Before
 	public void setUp() throws Exception {
+		DBHandler handler = DBHandler.getInstance();
+		String body = "{ \"firstName\": \"Andrea\", \"surname\": \"Novotna\", \"age\": \"19\", \"height\": \"150\", \"weight\": \"50\", \"votes\": \"0\", \"parameters\":{ \"chest\": \"200\", \"waist\": \"20\", \"hips\": \"100\" }";
+		String candidate = handler.createCandidate(body);
+		
 	}
 
 	@After
