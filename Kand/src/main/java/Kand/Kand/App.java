478c3d038d7b84b0766d402a505bc69977548859
Jan Berger <student@fakemail.com>
2014-11-19 11:11:53 +0100

    no message

---
+++ Kand/src/main/java/Kand/Kand/App.java
@@ -0,0 +1,13 @@
+package Kand.Kand;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
