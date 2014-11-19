f63ca74be08156ff60278d8b986e1ed92f726200
Tomas Prokes <tomas.prokes@centrum.cz>
2014-11-19 16:01:48 +0100

    DBHandlerTest.java - added

---
+++ src/backend/DBHandlerTest.java
@@ -0,0 +1,54 @@
+package backend;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DBHandlerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetCandidates() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testVote() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCandidate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateCandidate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveCandidate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateCandidate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetInstance() {
+		fail("Not yet implemented");
+	}
+
+}
