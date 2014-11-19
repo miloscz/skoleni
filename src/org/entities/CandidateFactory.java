c91b99a555ee7acef92d5f0e088f0ff5975c2e64
jiřiN <student@fakemail.com>
2014-11-19 14:45:04 +0100

    new method parse in CandidateFactory

--- src/org/entities/CandidateFactory.java
+++ src/org/entities/CandidateFactory.java
@@ -1,5 +1,9 @@
 package org.entities;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CandidateFactory {
 
 	public static Candidate createCandidate(String firstName, String surname, 
@@ -9,5 +13,10 @@ public class CandidateFactory {
 		return new Candidate(firstName, surname, age, chest, waist, hips, heigth, weight,votes);
 	}
 	
+	public List<Candidate> parse(String json){
+		
+		
+		return new ArrayList<>();
+	}
 	
 }
