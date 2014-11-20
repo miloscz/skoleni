6c054ba295c5aea303af621cbd67adc0fc5a5db0
Jan Berger <student@fakemail.com>
2014-11-20 10:59:30 +0100

    no message

---
+++ mongo/src/main/java/cz/unicorn/mongo/rest/DBRequestException.java
@@ -0,0 +1,9 @@
+package cz.unicorn.mongo.rest;
+
+public class DBRequestException extends DBException{
+
+	public DBRequestException(String msg) {
+		super(msg);
+	}
+
+}
---
+++ mongo/src/main/java/cz/unicorn/mongo/rest/DBServerException.java
@@ -0,0 +1,10 @@
+package cz.unicorn.mongo.rest;
+
+public class DBServerException  extends DBException {
+
+	public DBServerException(String msg) {
+		super(msg);
+		// TODO Auto-generated constructor stub
+	}
+
+}
